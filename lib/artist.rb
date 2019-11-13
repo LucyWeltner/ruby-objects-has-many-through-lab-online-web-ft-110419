4b0e01ebb5117bc151a9479fa0148d4888c7fbbc
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-13 22:10:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,11 @@ class Artist
     @@all_artists << self 
   end 
   
+  def new_song(name)
+    song = Song.new(name)
+    song.artist = self 
+  end
+  
   def songs 
     Song.all.select{|song| song.artist == self}
   end 
@@ -13,4 +18,8 @@ class Artist
   def genres 
     songs.collect{|song| song.genre}
   end 
+  
+  def self.all
+    @@all_artists 
+  end
 end

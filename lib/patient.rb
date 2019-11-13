d607be78ea8e31d54b5ea5e415693fc7211e547e
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-13 22:32:13 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,7 +12,7 @@ class Patient
   end 
   
   def appointments
-    Appointment.all.map{|appt| appt.patient == self}
+    Appointment.all.find_all{|appt| appt.patient == self}
   end 
   
   def doctors 
